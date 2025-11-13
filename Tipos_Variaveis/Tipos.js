a24fa2e17028e1caf3136aa4c6c98f1d7cc647f2
Lucas Vinícius <vinicioslucas37@hotmail.com>
2025-11-13 13:56:39 -0300

    feat: adiciona variáveis

---
+++ Tipos_Variaveis/Tipos.js
@@ -0,0 +1,11 @@
+/*
+    Tipos de dados em Js: undefined, null, boolean, string
+    symbol, number, object
+*/
+
+var nome = "Lucas"; //Recebe valor do tipo strig
+
+let matricula = 2827; //Recebe valor do tipo int
+
+
+
